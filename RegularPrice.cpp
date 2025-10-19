8547e44e29a2e9ced4aec08944511794ed27263b
Sabira-k-commits <sabira.k200416@gmail.com>
2025-10-19 10:15:20 +0200

    Regular price for customer
    Customers with no coupon number of plants being bought is below 5

---
+++ RegularPrice.cpp
@@ -0,0 +1,11 @@
+#include"RegularPrice.h"
+
+RegularPrice::RegularPrice(){}
+
+RegularPrice::~RegularPrice(){}
+
+double RegularPrice::calculatePrice(int numberOfPlants,double basePrice,std::string coupon){
+    std::cout<<"Regular Price applied: ";
+    return basePrice;
+}
+
