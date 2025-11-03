65db42b9f821bc339705a69b2f4bd86ae717899d
Sabira-k-commits <sabira.k200416@gmail.com>
2025-11-03 09:35:29 +0200

    Fixed segfaults on getting help from staff

--- SystemFiles/CustomerQueryHandler.cpp
+++ SystemFiles/CustomerQueryHandler.cpp
@@ -8,10 +8,10 @@ CustomerQueryHandler::~CustomerQueryHandler(){
     // Delete the next handler in the chain (if any) to free the whole chain
     // This assumes ownership semantics: the head owns the rest of the chain.
     ///just delete this if give you
-    /*if (nextHandler) {
+    if (nextHandler) {
         delete nextHandler;
         nextHandler = nullptr;
-    }*/
+    }
 }
 
 void CustomerQueryHandler::setNext(CustomerQueryHandler *handler){
--- SystemFiles/JuniorStaff.cpp
+++ SystemFiles/JuniorStaff.cpp
@@ -19,7 +19,7 @@ void JuniorStaff::handleQuery(const CustomerQuery &query){
             if(query.question.find("rose") != std::string::npos) {
                 std::cout << " Roses start at R450 each, or R2200 for a bouquet of six." << std::endl;
             } else if(query.question.find("orchid") != std::string::npos) {
-                std::cout << " Orchids range from R650 to R1500 depending on the variety." << std::endl;
+                std::cout << " Orchids range from R650 to R1500 depending on the variety." << std::endl;    
             } else if(query.question.find("student discount") != std::string::npos) {
                 std::cout << " We offer 10% student discount with valid ID!" << std::endl;
             } else {
@@ -36,9 +36,15 @@ void JuniorStaff::handleQuery(const CustomerQuery &query){
 
 bool JuniorStaff::canHandle(const CustomerQuery &query) const{
     if (query.type == CustomerQuery::GENERAL) {
-        return query.question.find("opening hours") != std::string::npos || query.question.find("location") != std::string::npos || query.question.find("gift wrapping") != std::string::npos;
+        return query.question.find("opening hours") != std::string::npos || 
+               query.question.find("location") != std::string::npos || 
+               query.question.find("gift wrapping") != std::string::npos;
     } else if (query.type == CustomerQuery::PRICING) {
-        return query.question.find("rose") != std::string::npos || query.question.find("orchid") != std::string::npos || query.question.find("student discount") != std::string::npos;
-    }
+        return query.question.find("rose") != std::string::npos || 
+               query.question.find("orchid") != std::string::npos || 
+               query.question.find("discount") != std::string::npos ||
+               query.question.find("bulk") != std::string::npos ||
+               query.question.find("student discount") != std::string::npos;
+    } 
     return false;
 }
